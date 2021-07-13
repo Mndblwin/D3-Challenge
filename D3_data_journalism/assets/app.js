fabcdd0a702da5b413df61b47259f58fa2b8af00
kerry.olden@gmail.com <kerry.olden@gmail.com>
2021-07-12 22:18:07 -0500

    final updates

--- D3_data_journalism/assets/app.js
+++ D3_data_journalism/assets/app.js
@@ -108,7 +108,7 @@ d3.csv("data.csv").then(function(censusData) {
   // Create axes labels
   chartGroup.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left + 40)
+    .attr("y", 0 - margin.left)
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .attr("class", "axisText")
